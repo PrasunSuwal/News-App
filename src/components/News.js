cbb74b6a287d3d25b84024a22546cc57ef637bfe
Prasun Suwal <prasun.suwal@gmail.com>
2024-08-12 10:43:11 +0545

    starting

---
+++ src/components/News.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    articles =  [
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+            "publishedAt": "2020-04-27T11:41:47Z",
+            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
+        },
+        {
+            "source": {
+              "id": "abc-news-au",
+              "name": "ABC News (AU)"
+            },
+            "author": "Samantha Lewis",
+            "title": "Obrigado, Marta — Brazilian legend makes way for the future after Paris Olympics final",
+            "description": "Having watched her young teammates battle through to an Olympic final, football's greatest leaves Brazil in a far better place than when she found it.",
+            "url": "https://www.abc.net.au/news/2024-08-11/obrigado-marta-brazil-legend-future-paris-olympics-final/104210668",
+            "urlToImage": "https://live-production.wcms.abc-cdn.net.au/76f66948bc5221122d790e11a6486e41?impolicy=wcms_watermark_news&cropH=2731&cropW=4855&xPos=0&yPos=82&width=862&height=485&imformat=generic",
+            "publishedAt": "2024-08-10T22:37:00Z",
+            "content": "As she stepped up onto the podium at the Parc des Princes, hand-in-hand with teammates half her age, Marta, the captain of Brazil, looked out across the pitch and, with a deep breath, smiled.\r\nThe co… [+8951 chars]"
+          },
+          {
+            "source": {
+              "id": "bbc-sport",
+              "name": "BBC Sport"
+            },
+            "author": null,
+            "title": "Emma Hayes: Olympics football gold with USA 'greatest moment' of career for former Chelsea manager",
+            "description": "Emma Hayes says winning Olympic football gold with the United States is \"the greatest moment\" of her managerial career.",
+            "url": "http://www.bbc.co.uk/sport/olympics/articles/c24pn74125eo",
+            "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_sport/6d75/live/3a482680-574d-11ef-99a2-f7ec1ec76beb.jpg",
+            "publishedAt": "2024-08-10T21:37:17.6981828Z",
+            "content": "When the final whistle blew, Hayes was overcome with emotion as she embraced her staff on the touchline before jumping up and down in celebration with her players.\r\nThroughout the tournament, Hayes h… [+890 chars]"
+          },
+        {
+            "source": {
+                "id": "espn-cric-info",
+                "name": "ESPN Cric Info"
+            },
+            "author": null,
+            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+            "publishedAt": "2020-03-30T15:26:05Z",
+            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+        }
+    ]
+    constructor(){
+    super();
+    console.log("I am a constructor for news components");
+    this.state ={
+        articles: this.articles,
+        loading: false 
+    }
+  }
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>Newzzz - Top Headlines</h2>
+        <div className="row">
+            {this.state.articles.map((element)=>{
+               return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title.slice(0,45)} description={element.description.slice(0,69)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            </div>
+            })}   
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
+
